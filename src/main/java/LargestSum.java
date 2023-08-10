1eb1c0101b285025c95c76cfa78bdc154da19322
CANSU TEKIN <ctekin22@gmail.com>
2023-08-10 19:45:49 +0000

    MaxSum implemented.

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,16 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int max = 0;
+        int maxsum=0;
+        for (int i =0; i< nums.size(); i++){
+            for (int j =i+1; j < nums.size(); j++){
+                maxsum = nums.get(i) + nums.get(j);
+                if(maxsum>max){
+                    max=maxsum;
+                }
+            }       
+        }
+        return max;
     }
 }
